dd9efe2dbc9954b24d481b24d1c7717bf228bcac
v1nter <sebastiankleinhaltern@gmail.com>
2023-12-06 00:14:54 +0100

    Cookie Consent

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import { HelmetProvider } from 'react-helmet-async';
+import CookieConsent from 'react-cookie-consent';
 
 import Wrapper from './components/Wrapper/Wrapper.jsx';
 import Landing from './components/Landing/Landing.jsx';
@@ -8,6 +9,9 @@ import Landing from './components/Landing/Landing.jsx';
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<StrictMode>
 		<HelmetProvider>
+			<CookieConsent>
+				This website uses cookies to enhance the user experience.
+			</CookieConsent>
 			<Wrapper />
 		</HelmetProvider>
 	</StrictMode>
